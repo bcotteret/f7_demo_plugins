6b048a8a0e720e7c49881e4f6655eb156b1db378
Bruno COTTERET <bcotteret@lm2s.fr>
2020-06-05 14:00:46 +0200

    Initial commit - default Framework7 App skeleton

---
+++ src/components/app.vue
@@ -0,0 +1,57 @@
+<template>
+  <f7-app :params="f7params" >
+
+  <!-- Your main view, should have "view-main" class -->
+  <f7-view main class="safe-areas" url="/"></f7-view>
+
+  </f7-app>
+</template>
+<script>
+  import { Device }  from 'framework7/framework7-lite.esm.bundle.js';
+  import cordovaApp from '../js/cordova-app.js';
+  import routes from '../js/routes.js';
+
+  export default {
+    data() {
+      return {
+        // Framework7 Parameters
+        f7params: {
+          id: 'io.f7.demo.plugins', // App bundle ID
+          name: 'f7-demo', // App name
+          theme: 'auto', // Automatic theme detection
+
+
+
+          // App routes
+          routes: routes,
+
+          // Register service worker
+          serviceWorker: Device.cordova ? {} : {
+            path: '/service-worker.js',
+          },
+          // Input settings
+          input: {
+            scrollIntoViewOnFocus: Device.cordova && !Device.electron,
+            scrollIntoViewCentered: Device.cordova && !Device.electron,
+          },
+          // Cordova Statusbar settings
+          statusbar: {
+            iosOverlaysWebView: true,
+            androidOverlaysWebView: false,
+          },
+        },
+
+      }
+    },
+
+    mounted() {
+      this.$f7ready((f7) => {
+        // Init cordova APIs (see cordova-app.js)
+        if (Device.cordova) {
+          cordovaApp.init(f7);
+        }
+        // Call F7 APIs here
+      });
+    }
+  }
+</script>
